6442e647aa3cc4fe71c69dda7e1206a9d9a55961
Dan Oneata <dan.oneata@gmail.com>
2021-08-07 12:33:56 +0300

    ENH Group true and pred by image key

--- scripts/evaluate_visual_vs_text_qualitative.py
+++ scripts/evaluate_visual_vs_text_qualitative.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 from matplotlib import pyplot as plt
 from sklearn.metrics import auc, roc_curve, precision_recall_curve
-from toolz import partition
+from toolz import compose, first, groupby, partition, second
 
 from scripts.evaluate import (
     MODELS,
@@ -38,8 +38,20 @@ def wav_path_to_key(wav_path):
     return key
 
 
-def show1(data, true, pred, word):
-    word_id = data["VOCAB"][word]
+def get_key_img(key):
+    *parts, _ = key.split("_")
+    return "_".join(parts)
+
+
+def reverse_non_injective_dict(d):
+    q = {}
+    for k, v in d.items():
+        q.setdefault(v, []).append(k)
+    return q
+
+
+def show1(true, pred, vocab, word, prepare_sample):
+    word_id = vocab[word]
     true1 = true[:, word_id]
     pred1 = pred[:, word_id]
 
@@ -65,33 +77,12 @@ def show1(data, true, pred, word):
     col1.markdown("## Quantitative results")
     col1.pyplot(fig)
 
-    def prepare_data(sample_id, rank):
-        sample = data["test"][sample_id]
-        wav_path = sample["wave"]
-        img_path = wav_to_img_path(wav_path)
-
-        key = wav_path_to_key(wav_path)
-        transcript = data["key_to_transcript"][key]
-        words_sample = [data["vocab_inv"][w] for w in np.where(true[sample_id])[0]]
-        description = "\n".join(
-            [
-                "key:        " + key,
-                "rank:       " + str(rank),
-                "is-correct: " + str(bool(true1[sample_id])),
-                "score:      " + "{:.2f}".format(pred1[sample_id]),
-                "labels:     " + ", ".join(words_sample),
-                "transcript: " + transcript,
-            ]
-        )
-
-        return img_path, description
-
     st.markdown("## Top 10 predictions")
     ranks = np.arange(num_samples)
     rank_to_sample_id = pred1.argsort()[::-1]
     samples_top10 = rank_to_sample_id[:10]
     data_top10 = [
-        prepare_data(sample_id, rank + 1)
+        prepare_sample(word_id, sample_id, rank + 1)
         for rank, sample_id in zip(ranks[:10], samples_top10)
     ]
     for group in partition(5, data_top10):
@@ -106,7 +97,7 @@ def show1(data, true, pred, word):
     rank_eq_true_5 = rank_eq_true[-5:]
     samples_bot5pos = rank_to_sample_id[rank_eq_true_5]
     data_bot5 = [
-        prepare_data(sample_id, rank + 1)
+        prepare_sample(word_id, sample_id, rank + 1)
         for rank, sample_id in zip(ranks[rank_eq_true_5], samples_bot5pos)
     ]
     columns = st.beta_columns(5)
@@ -116,17 +107,19 @@ def show1(data, true, pred, word):
 
 
 def main():
+    TO_SHOW_SINGLE_WORD = True
+
     with open(os.path.join(BASE_PATH, config.pickle_file), "rb") as f:
         data = pickle.load(f)
 
     file_transcript = os.path.join(config.flickr8k_trans_dir, "Flickr8k.token.txt")
     key_to_transcript = dict(load(file_transcript, parse_token))
-    data["key_to_transcript"] = key_to_transcript
 
     vocab = data["VOCAB"]
+    samples = data["test"]
+
     vocab_inv = {i: w for w, i in vocab.items()}
     words = [vocab_inv[i] for i in range(len(vocab))]
-    data["vocab_inv"] = vocab_inv
 
     st.set_page_config(layout="wide")
     st.title("Visual predictions versus textual labels on Flickr8K")
@@ -134,15 +127,86 @@ def main():
     col1, _ = st.beta_columns(2)
 
     col1.markdown("## Options")
+    to_group = col1.checkbox("group by image:")
     model_name = col1.selectbox("model: ", MODELS)
+
+    if TO_SHOW_SINGLE_WORD:
+        word = col1.selectbox("word: ", words)
 
     true = load_true(data)
     pred = MODELS[model_name](data)
 
-    for word in words:
-        st.markdown("# " + word)
-        show1(data, true, pred, word)
-        st.markdown("---")
+    def prepare_sample(word_id, sample_id, rank):
+        sample = samples[sample_id]
+        wav_path = sample["wave"]
+        img_path = wav_to_img_path(wav_path)
+
+        key = wav_path_to_key(wav_path)
+        transcript = key_to_transcript[key]
+        words_sample = [vocab_inv[w] for w in np.where(true[sample_id])[0]]
+        description = "\n".join(
+            [
+                "key:        " + key,
+                "rank:       " + str(rank),
+                "is-correct: " + str(bool(true[sample_id, word_id])),
+                "score:      " + "{:.2f}".format(pred[sample_id, word_id]),
+                "labels:     " + ", ".join(words_sample),
+                "transcript: " + transcript,
+            ]
+        )
+
+        return img_path, description
+
+    if to_group:
+        # Group groundtruth and predictions corresponding to the same image.
+        # Groundtruth is aggergated by the `or` function, while the predictions
+        # should all be the same, since they are based only on the input image.
+
+        key_to_key_img = {key: get_key_img(key) for key in key_to_transcript.keys()}
+        key_img_to_keys = reverse_non_injective_dict(key_to_key_img)
+
+        indices_keys = enumerate(wav_path_to_key(sample["wave"]) for sample in samples)
+        indices_keys_groups = groupby(compose(get_key_img, second), indices_keys)
+
+        keys_img = sorted(indices_keys_groups.keys())
+        indices_groups = [list(map(first, indices_keys_groups[k])) for k in keys_img]
+
+        true = np.vstack([np.any(true[ids], axis=0) for ids in indices_groups])
+        pred = np.vstack([pred[ids][0] for ids in indices_groups])
+
+        def prepare_sample(word_id, sample_id, rank):
+            key_img = keys_img[sample_id]
+            img_path = os.path.join(
+                config.BASE_DIR,
+                "flickr8k-images",
+                "Flicker8k_Dataset",
+                key_img + ".jpg",
+            )
+
+            keys = key_img_to_keys[key_img]
+            transcripts = "\n".join("\t" + key_to_transcript[key] for key in keys)
+            words_sample = [vocab_inv[w] for w in np.where(true[sample_id])[0]]
+            description = "\n".join(
+                [
+                    "img-key:     " + key_img,
+                    "keys:        " + ", ".join(keys),
+                    "rank:        " + str(rank),
+                    "is-correct:  " + str(bool(true[sample_id, word_id])),
+                    "score:       " + "{:.2f}".format(pred[sample_id, word_id]),
+                    "labels:      " + ", ".join(words_sample),
+                    "transcripts: " + "\n" + transcripts,
+                ]
+            )
+
+            return img_path, description
+
+    if TO_SHOW_SINGLE_WORD:
+        show1(true, pred, vocab, word, prepare_sample)
+    else:
+        for word in words:
+            st.markdown("# " + word)
+            show1(true, pred, vocab, word, prepare_sample)
+            st.markdown("---")
 
 
 if __name__ == "__main__":
