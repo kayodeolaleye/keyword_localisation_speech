832526738d453e2c7461ca286667c6d609e6d7bc
Dan Oneata <dan.oneata@gmail.com>
2021-08-21 21:25:36 +0300

    WIP Train in the embedding space

--- Interspeech/predict_emb.py
+++ Interspeech/predict_emb.py
@@ -2,12 +2,14 @@ import os
 import pdb
 
 from functools import partial
-from typing import Dict
+from typing import Dict, Sequence, Tuple
 
 import click
 import numpy as np
 
 from toolz import first
+
+import clip
 
 import torch
 from torch.utils.data import DataLoader
@@ -15,7 +17,7 @@ from torch.utils.data import DataLoader
 from ignite.handlers.stores import EpochOutputStore
 from ignite.contrib.metrics import AveragePrecision
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
-from ignite.engine import Events, create_supervised_evaluator
+from ignite.engine import Engine, Events, _prepare_batch, create_supervised_evaluator
 from ignite.metrics import Metric
 
 import config
@@ -25,10 +27,11 @@ from train_emb import (
     EMBED_SIZE,
     HPARAMS,
     OUTPUT_DIR,
+    OUT_DIM,
     TARGET_LOADERS,
     VOCAB_SIZE,
     Flickr8kDataset,
-    output_transform_metric,
+    # output_transform_metric,
     pad_collate,
 )
 
@@ -36,12 +39,12 @@ from train_emb import (
 def load_model(audio_model_name, teacher_model_name):
     prefix = "{}-{}".format(audio_model_name, teacher_model_name)
     files = [f for f in os.listdir(OUTPUT_DIR) if f.startswith(prefix)]
-    assert len(files) == 1
+    # assert len(files) == 1
 
     path = os.path.join(OUTPUT_DIR, first(files))
 
-    model = AUDIO_MODELS[audio_model_name](VOCAB_SIZE, EMBED_SIZE)
-    model.load_state_dict(torch.load(path))
+    model = AUDIO_MODELS[audio_model_name](OUT_DIM)
+    model.load_state_dict(torch.load(path)["model"])
     model.to(config.device)
 
     return model
@@ -63,32 +66,65 @@ def load_model(audio_model_name, teacher_model_name):
     type=click.Choice(TARGET_LOADERS),
 )
 def main(audio_model_name, teacher_model_name):
-    BATCH_SIZE = 256
+    assert teacher_model_name == "features-image-clip" and audio_model_name == "cnn-transformer"
+
+    BATCH_SIZE = 64
+    dataset = Flickr8kDataset(split="test", target_type="labels-text", is_train=False)
     loader = DataLoader(
-        Flickr8kDataset(split="test", target_type="labels-text", is_train=False),
+        dataset,
         batch_size=BATCH_SIZE,
         shuffle=False,
         collate_fn=partial(pad_collate, dim=1),
     )
 
+    text_template = "a photo of a {word}"
+    vocab = dataset.load_target.vocab
+    id_to_word = {i: w for w, i in vocab.items()}
+    words = [id_to_word[i] for i in range(len(vocab))]
+
+    texts = torch.cat([clip.tokenize(text_template.format(word=w)) for w in words])
+    texts = texts.to(config.device)
+
+    clip_model, preprocess = clip.load("ViT-B/16", config.device)
+
+    with torch.no_grad():
+        text_features = clip_model.encode_text(texts)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+        text_features = text_features.type(torch.float32)
+
+    def evaluate_step(engine: Engine, batch: Sequence[torch.Tensor]) -> Tuple:
+        model.eval()
+        with torch.no_grad():
+            x, y = _prepare_batch(batch, device=config.device)
+            y_pred = model.embed_audio(x)
+            return x, y, y_pred
+
+    def output_transform_metric(inp_true_pred):
+        _, true, pred = inp_true_pred
+        speech_features = pred / pred.norm(dim=-1, keepdim=True)
+        similarity = speech_features @ text_features.T
+        return similarity, true
+
     metrics: Dict[str, Metric] = {
-        "aupr": AveragePrecision(output_transform=output_transform_metric),
+        "aupr": AveragePrecision(output_transform_metric),
     }
     model = load_model(audio_model_name, teacher_model_name)
-    evaluator = create_supervised_evaluator(model, metrics, device=config.device)
 
-    def output_transform(output_and_true):
-        output, true = output_and_true
-        pred, _ = output
-        return true, pred
+    evaluator = Engine(evaluate_step)
+    for name, metric in metrics.items():
+        metric.attach(evaluator, name)
 
-    eos = EpochOutputStore(output_transform=output_transform)
+    def output_transform(inp_true_pred):
+        _, true, pred = inp_true_pred
+        return pred.cpu().numpy(), true.cpu().numpy()
+
+    eos = EpochOutputStore(output_transform)
     eos.attach(evaluator, "output")
 
     @evaluator.on(Events.EPOCH_COMPLETED)
     def save_results(engine):
-        pred = np.vstack([output[0].cpu() for output in engine.state.output])
-        true = np.vstack([output[1].cpu() for output in engine.state.output])
+        pred = np.vstack([output[0] for output in engine.state.output])
+        true = np.vstack([output[1] for output in engine.state.output])
         path = f"output/{audio_model_name}-{teacher_model_name}-flickr8k-test.npy"
         np.savez(path, pred=pred, true=true)
 
--- Interspeech/train_emb.py
+++ Interspeech/train_emb.py
@@ -4,14 +4,16 @@
 # - [x] fix random seed
 # - [x] run training for `labels-text` and `labels-image-vgg` configurations
 # - [x] write script to predict based on a model
-# - [ ] write script to extract CLIP features
+# - [x] write script to extract CLIP features
 # - [ ] train on CLIP features
-# - [ ] (optional) early stopping
+# - [ ] write output transform function for CLIP
+# - [ ] resume from checkpoint
 # - [ ] (optional) learning rate scheduler
 
 import os
 import pdb
 import pickle
+import random
 
 from itertools import groupby
 from functools import partial
@@ -25,15 +27,29 @@ import numpy as np
 import torch
 import torch.nn
 
-from torch.nn.functional import binary_cross_entropy, mse_loss
+from torch.nn.functional import binary_cross_entropy, cross_entropy, mse_loss
 from torch.utils.data import DataLoader, Dataset
 
 from ignite.contrib.metrics import AveragePrecision
-from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
-from ignite.handlers import ModelCheckpoint
+from ignite.engine import (
+    Events,
+    create_supervised_trainer,
+    create_supervised_evaluator,
+    _prepare_batch,
+)
+from ignite.handlers import (
+    ModelCheckpoint,
+    create_lr_scheduler_with_warmup,
+    CosineAnnealingScheduler,
+)
 from ignite.metrics import Loss, Metric
 
+import streamlit as st
+from matplotlib import pyplot as plt
+
 from toolz import compose, concat, first
+
+from clip.model import Transformer
 
 import config
 from data_gen import spec_augment
@@ -42,25 +58,82 @@ from utils import extract_feature, get_soft_tags, get_keywords, get_tran_dict
 from scripts.data import get_key_img, load, parse_token
 
 
+# Types for mypy
 Split = Literal["train", "dev", "test"]
 TeacherType = Literal["labels-image-vgg", "labels-text", "features-image-clip"]
-
 
 # Hyper-parameters
 HPARAMS: Dict[str, Any] = {
     "batch-size": 32,
-    "lr": 1e-4,
-    "max-epochs": 100,
+    "lr": 4 * 1e-4,
+    "num-gradient-updates": 25_000,
+    "num-warmup-steps": 1_000,
     "max-len-audio": 2048,
     "seed": 42,
 }
 
+# Constants
 VOCAB_SIZE = 67
 EMBED_SIZE = 1000
+OUT_DIM = 512
 OUTPUT_DIR = "trained_models"
 
+LOG_TRAIN_FREQ = 16
+LOG_VALID_FREQ = 256
 
+# Fix random seed
+random.seed(HPARAMS["seed"])
+np.random.seed(HPARAMS["seed"])
 torch.manual_seed(HPARAMS["seed"])
+
+
+class CNNTransformer(torch.nn.Module):
+    def __init__(self, embed_dim=512, **kwargs):
+        super().__init__()
+        # Convolutional module
+        width = 128
+        self.conv = torch.nn.Sequential(
+            torch.nn.Conv1d(39, 96, 9, 2, 4),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, 96, 11, 1, 5),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, 96, 11, 2, 5),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, 96, 11, 1, 5),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, 96, 11, 2, 5),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, 96, 11, 1, 5),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(96, width, 11, 2, 5),
+        )
+        self.transformer = Transformer(width=width, layers=8, heads=4)
+        self.ln_final = torch.nn.LayerNorm(width)
+        self.proj = torch.nn.Linear(width, embed_dim)
+        self.logit_scale = torch.nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+    def embed_audio(self, audio):
+        x = self.conv(audio)
+        x = x.permute(2, 0, 1)  # BDT → TBD
+        x = self.transformer(x)
+        x = x.permute(1, 2, 0)  # TBD → BDT
+        x = self.ln_final(x[:, :, 0])
+        x = self.proj(x)
+        return x
+
+    def forward(self, audio_and_target):
+        audio, target = audio_and_target
+        input_ = self.embed_audio(audio)
+        inp_features = input_ / input_.norm(dim=-1, keepdim=True)
+        out_features = target / target.norm(dim=-1, keepdim=True)
+        τ = torch.clamp(self.logit_scale.exp(), 0, 100)
+        logits = τ * inp_features @ out_features.T
+        return logits
+
+
+AUDIO_MODELS = {
+    "cnn-transformer": CNNTransformer,
+}
 
 
 def pad_tensor(vec, pad, dim):
@@ -74,19 +147,6 @@ def pad_collate(batch, dim=0):
     xs = torch.stack([pad_tensor(xy[0], max_len, dim) for xy in batch], dim=0)
     ys = torch.stack([xy[1] for xy in batch])
     return xs, ys
-
-
-def output_transform_network(output):
-    # Assume that the network:
-    # ⅰ. the network outputs extra information, which is discarded;
-    # ⅱ. the predictions correpsond to logits.
-    pred, _ = output
-    return torch.sigmoid(pred)
-
-
-def output_transform_metric(output_and_true):
-    output, true = output_and_true
-    return output_transform_network(output), true
 
 
 class LabelsImageVGGLoader:
@@ -103,7 +163,7 @@ class LabelsImageVGGLoader:
         )
 
 
-class LabelsText:
+class LabelsTextLoader:
     def __init__(self):
         path_tran_dict = os.path.join(config.flickr8k_folder, "tran_dict.pkl")
         with open(path_tran_dict, "rb") as f:
@@ -119,10 +179,23 @@ class LabelsText:
         return bow_vector
 
 
+class FeaturesImageCLIPLoader:
+    def __init__(self):
+        path = "output/features/clip-ViT-B-16.npz"
+        data = np.load(path)
+        self.name_to_index = {n: i for i, n in enumerate(data["samples"])}
+        self.data = data["data"].astype(np.float32)
+
+    def __call__(self, sample_name):
+        key_img = get_key_img(sample_name)
+        index = self.name_to_index[key_img]
+        return self.data[index]
+
+
 TARGET_LOADERS = {
     "labels-image-vgg": LabelsImageVGGLoader,
-    "labels-text": LabelsText,
-    "features-image-clip": None,
+    "labels-text": LabelsTextLoader,
+    "features-image-clip": FeaturesImageCLIPLoader,
 }
 
 
@@ -147,7 +220,8 @@ class Flickr8kDataset(Dataset):
         img_key_to_keys = {
             k: list(g) for k, g in groupby(sorted(keys), key=get_key_img)
         }
-        return list(concat(img_key_to_keys[img_key] for img_key in img_keys))
+        res = list(concat(img_key_to_keys[img_key] for img_key in img_keys))
+        return res
 
     def load_audio_features(self, sample_name):
         audio_path = os.path.join(
@@ -196,6 +270,125 @@ def get_data_loaders(teacher_model_name, batch_size):
     return train_loader, valid_loader
 
 
+# @click.command()
+# @click.option(
+#     "-a",
+#     "--audio-model",
+#     "audio_model_name",
+#     required=True,
+#     type=click.Choice(AUDIO_MODELS),
+# )
+# @click.option(
+#     "-t",
+#     "--teacher",
+#     "teacher_model_name",
+#     required=True,
+#     type=click.Choice(TARGET_LOADERS),
+# )
+# def main(audio_model_name, teacher_model_name):
+#
+#     target_type, *_ = teacher_model_name.split("-")
+#
+#     train_loader, valid_loader = get_data_loaders(
+#         teacher_model_name,
+#         HPARAMS["batch-size"],
+#     )
+#
+#     if target_type == "features":
+#         VOCAB_SIZE = 512
+#     else:
+#         VOCAB_SIZE = 67
+#
+#     model = AUDIO_MODELS[audio_model_name](VOCAB_SIZE, EMBED_SIZE)
+#     model.to(config.device)
+#
+#     # if teacher_model_name == "features-image-clip":
+#     #     model.τ = torch.nn.Parameter(1.0)
+#
+#     optimizer = torch.optim.Adam(model.parameters(), lr=HPARAMS["lr"])
+#
+#     def batch_cross_entropy(output, target):
+#         audio_features = output / output.norm(dim=-1, keepdim=True)
+#         image_features = target / target.norm(dim=-1, keepdim=True)
+#         logits = audio_features @ image_features.T
+#         batch_size, _ = output.shape
+#         batch_idxs = torch.arange(batch_size).to(config.device)
+#         # print(torch.softmax(logits, dim=0))
+#         return cross_entropy(logits, batch_idxs)
+#
+#     CRITERIA = {
+#         "labels": binary_cross_entropy,
+#         "features": batch_cross_entropy,
+#     }
+#
+#     def my_criterion(output, target):
+#         return CRITERIA[target_type](output[0], target)
+#         # return CRITERIA[target_type](output_transform_network(output), target)
+#
+#     trainer = create_supervised_trainer(
+#         model, optimizer, my_criterion, device=config.device
+#     )
+#
+#     valid_metrics: Dict[str, Metric] = {
+#         "loss": Loss(my_criterion),
+#     }
+#
+#    def output_transform_network(output):
+#        # Assume that the network:
+#        # ⅰ. the network outputs extra information, which is discarded;
+#        # ⅱ. the predictions correpsond to logits.
+#        pred, _ = output
+#        return torch.sigmoid(pred)
+#
+#
+#    def output_transform_metric(output_and_true):
+#        output, true = output_and_true
+#        return output_transform_network(output), true
+#
+#     if teacher_model_name == "labels-text":
+#         valid_metrics["aupr"] = AveragePrecision(
+#             output_transform=output_transform_metric
+#         )
+#
+#     evaluator = create_supervised_evaluator(
+#         model, metrics=valid_metrics, device=config.device
+#     )
+#
+#     @trainer.on(Events.ITERATION_COMPLETED(every=config.print_freq))
+#     def log_training_loss(trainer):
+#         print(
+#             "train · epoch: {:4d} ◇ loss: {:7.4f}".format(
+#                 trainer.state.epoch, trainer.state.output
+#             )
+#         )
+#
+#     @trainer.on(Events.EPOCH_COMPLETED)
+#     def log_validation_results(trainer):
+#         evaluator.run(valid_loader)
+#         metrics = evaluator.state.metrics
+#         print(
+#             "valid · epoch: {:4d} ◇".format(trainer.state.epoch),
+#             " ◇ ".join("{:s}: {:7.4f}".format(k, v) for k, v in metrics.items()),
+#         )
+#
+#     # Chekpoint
+#     prefix = "{}-{}".format(audio_model_name, teacher_model_name)
+#     checkpoint_handler = ModelCheckpoint(
+#         OUTPUT_DIR,
+#         prefix,
+#         n_saved=1,
+#         require_empty=False,
+#         score_function=lambda engine: -engine.state.metrics["loss"],
+#     )
+#     evaluator.add_event_handler(
+#         event_name=Events.EPOCH_COMPLETED,
+#         handler=checkpoint_handler,
+#         to_save={"model": model},
+#     )
+#
+#     trainer.run(train_loader, max_epochs=HPARAMS["max-epochs"])
+
+
 @click.command()
 @click.option(
     "-a",
@@ -220,57 +413,56 @@ def main(audio_model_name, teacher_model_name):
         HPARAMS["batch-size"],
     )
 
-    # TODO use target type information to strip the head
-    model = AUDIO_MODELS[audio_model_name](VOCAB_SIZE, EMBED_SIZE)
+    model = AUDIO_MODELS[audio_model_name](OUT_DIM)
     model.to(config.device)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=HPARAMS["lr"])
 
-    CRITERIA = {
-        "labels": binary_cross_entropy,
-        "features": mse_loss,
-    }
-
-    def my_criterion(output, target):
-        return CRITERIA[target_type](output_transform_network(output), target)
+    def prepare_batch(batch, device, non_blocking):
+        inp_out = _prepare_batch(batch, device, non_blocking)
+        indices = torch.arange(inp_out[0].shape[0]).to(config.device)
+        return inp_out, indices
 
     trainer = create_supervised_trainer(
-        model, optimizer, my_criterion, device=config.device
+        model,
+        optimizer,
+        cross_entropy,
+        prepare_batch=prepare_batch,
+        device=config.device,
     )
 
-    valid_metrics: Dict[str, Metric] = {
-        "loss": Loss(my_criterion),
+    validation_metrics: Dict[str, Metric] = {
+        "loss": Loss(cross_entropy),
     }
 
-    if teacher_model_name == "labels-text":
-        valid_metrics["aupr"] = AveragePrecision(
-            output_transform=output_transform_metric
-        )
-
     evaluator = create_supervised_evaluator(
-        model, metrics=valid_metrics, device=config.device
+        model,
+        metrics=validation_metrics,
+        prepare_batch=prepare_batch,
+        device=config.device,
     )
 
-    @trainer.on(Events.ITERATION_COMPLETED(every=config.print_freq))
+    @trainer.on(Events.ITERATION_COMPLETED(every=LOG_TRAIN_FREQ))
     def log_training_loss(trainer):
         print(
-            "train · epoch: {:4d} ◇ loss: {:7.4f}".format(
-                trainer.state.epoch, trainer.state.output
+            "train · step: {:5d} ◇ loss: {:7.4f}".format(
+                trainer.state.iteration, trainer.state.output
             )
         )
 
-    @trainer.on(Events.EPOCH_COMPLETED)
+    @trainer.on(Events.ITERATION_COMPLETED(every=LOG_VALID_FREQ))
     def log_validation_results(trainer):
         evaluator.run(valid_loader)
         metrics = evaluator.state.metrics
         print(
-            "valid · epoch: {:4d} ◇".format(trainer.state.epoch),
+            "valid · step: {:5d} ◇".format(trainer.state.iteration),
             " ◇ ".join("{:s}: {:7.4f}".format(k, v) for k, v in metrics.items()),
         )
 
     # Chekpoint
     prefix = "{}-{}".format(audio_model_name, teacher_model_name)
     checkpoint_handler = ModelCheckpoint(
-        output_dir,
+        OUTPUT_DIR,
         prefix,
         n_saved=1,
         require_empty=False,
@@ -279,10 +471,41 @@ def main(audio_model_name, teacher_model_name):
     evaluator.add_event_handler(
         event_name=Events.EPOCH_COMPLETED,
         handler=checkpoint_handler,
-        to_save={"model": model},
+        to_save={
+            "model": model,
+            "optimizer": optimizer,
+        },
     )
 
-    trainer.run(train_loader, max_epochs=HPARAMS["max-epochs"])
+    cycle_size = HPARAMS["num-gradient-updates"] - HPARAMS["num-warmup-steps"] + 1
+    scheduler_cosine = CosineAnnealingScheduler(
+        optimizer,
+        "lr",
+        start_value=HPARAMS["lr"],
+        end_value=0,
+        cycle_size=cycle_size,
+    )
+    lr_values = [None] * HPARAMS["num-gradient-updates"]
+    scheduler_cosine_warmup = create_lr_scheduler_with_warmup(
+        scheduler_cosine,
+        warmup_start_value=0.0,
+        warmup_end_value=HPARAMS["lr"],
+        warmup_duration=HPARAMS["num-warmup-steps"],
+        output_simulated_values=lr_values,
+    )
+
+    # Plot simulated values
+    # lr_values = np.array(lr_values)
+    # fig, ax = plt.subplots()
+    # ax.plot(lr_values[:, 0], lr_values[:, 1], label="learning rate")
+    # st.pyplot(fig)
+    # pdb.set_trace()
+
+    trainer.add_event_handler(Events.ITERATION_STARTED, scheduler_cosine_warmup)
+
+    num_batches = len(train_loader)  # number of gradient updates per epoch
+    max_epochs = int(HPARAMS["num-gradient-updates"] / num_batches)
+    trainer.run(train_loader, max_epochs=max_epochs)
 
 
 if __name__ == "__main__":
