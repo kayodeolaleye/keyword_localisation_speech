576f82d526797ce0cabdc7da6f8d9ee347cf7867
Dan Oneata <dan.oneata@gmail.com>
2021-08-31 14:56:03 +0300

    ENH Visualization

--- scripts/show_embedding_model_results.py
+++ scripts/show_embedding_model_results.py
@@ -1,90 +1,121 @@
+import json
+import random
+import pdb
 import sys
+
+from typing import Union
+from toolz import partition
 
 import torch
 
 import numpy as np
+import pandas as pd
 import streamlit as st
+
+from itertools import product
+
+from scripts.evaluate_emb_retrieval import (
+    KeyAudio,
+    KeyImage,
+    MODALITIES,
+    evaluate,
+    get_key_img,
+    load_data,
+)
 
 BASE_PATH = "Interspeech"
 sys.path.append(BASE_PATH)
-from train_emb import FeaturesImageCLIPLoader, Flickr8kDataset, get_key_img
+from train_emb import (
+    FeaturesImageCLIPLoader,
+    FeaturesAudioCLIPLoader,
+    Flickr8kDataset,
+    get_key_img,
+)
 
 
-class FeaturesAudioCLIPLoader:
-    def __init__(self):
-        path = "Interspeech/output/cnn-transformer-features-image-clip-flickr8k-test.npz"
-        data = np.load(path)
-        samples = Flickr8kDataset.load_samples("test")
-        self.name_to_index = {n: i for i, n in enumerate(samples)}
-        self.pred = data["pred"]
+def evaluate_all_combinations():
+    results = {
+        "`{:5s} → {:5s}`".format(src, tgt): evaluate(*load_data(src, tgt))
+        for src, tgt in product(MODALITIES, MODALITIES)
+        if src != tgt
+    }
 
-    def __call__(self, sample_name):
-        index = self.name_to_index[sample_name]
-        return self.pred[index]
+    df = pd.DataFrame(results)
+    kwargs = {
+        "floatfmt": ".1f",
+        "tablefmt": "github",
+    }
+    st.code(df.transpose().to_markdown(**kwargs))
 
 
-def audio_to_image_retrieval(to_shuffle=True):
+def show_retrieval(to_shuffle=True, k=5):
+    src = st.selectbox("source modality", MODALITIES, index=0)
+    tgt = st.selectbox("target modality", MODALITIES, index=1)
+
+    src_samples, tgt_samples, sim, are_same = load_data(src, tgt)
     transcripts = Flickr8kDataset.load_transcripts()
-    samples = Flickr8kDataset.load_samples("test")
-    samples_img = sorted(set([get_key_img(s) for s in samples]))
 
-    if to_shuffle:
-        import random
-        random.shuffle(samples)
+    def get_text(sample: Union[KeyAudio, KeyImage]):
+        if isinstance(sample, KeyAudio):
+            return transcripts[sample.value]
+        elif isinstance(sample, KeyImage):
+            return "\n" + "\n".join(
+                "\t" + transcripts[sample.to_key_audio(i).value] for i in range(5)
+            )
 
-    audio_loader = FeaturesAudioCLIPLoader()
-    image_loader = FeaturesImageCLIPLoader(BASE_PATH)
-
-    audio_emb = torch.vstack([torch.tensor(audio_loader(s)) for s in samples])
-    image_emb = torch.vstack([torch.tensor(image_loader(s + "_0")) for s in samples_img])
-    
-    audio_emb = audio_emb / audio_emb.norm(dim=-1, keepdim=True)
-    image_emb = image_emb / image_emb.norm(dim=-1, keepdim=True)
-
-    sim = audio_emb @ image_emb.T
-    sim = sim.numpy()
-
-    st.set_page_config(layout="wide")
-    st.title("retrieval · audio → image")
-    st.markdown("""
-        - for each audio utterance in the test set, retrieve the top five most similar images
+    st.markdown(f"## retrieval · {src} → {tgt}")
+    st.markdown(
+        f"""
+        - for each {src} sample in the test set, we retrieve the top {k} most similar {tgt} samples
         - use audio model trained in the embedding space with images represented by CLIP features
-    """)
+        - below we show 16 random samples
+    """
+    )
 
-    for i, sample in enumerate(samples[:64]):
-        top5 = sim[i].argsort()[::-1][:5]
-        is_correct = any(get_key_img(sample) == samples_img[r] for r in top5)
+    indices = list(range(len(src_samples)))
+    if to_shuffle:
+        random.shuffle(indices)
+    indices = indices[:16]
+
+    for i in indices:
+        s = src_samples[i]
+        top_indices = sim[i].argsort()[::-1][:k]
+        is_correct = any(are_same(s, tgt_samples[r]) for r in top_indices)
 
         desc = [
-            "text:     " + transcripts[sample],
-            "in-top-5: " + str(is_correct),
+            f"in-top-{k}: " + str(is_correct),
+            f"text:       " + get_text(s),
         ]
 
         col, *_ = st.beta_columns([0.4, 0.6])
-        col.markdown("## " + sample)
-        col.image(Flickr8kDataset.get_image_path(sample))
-        col.audio(Flickr8kDataset.get_audio_path(sample))
+        col.markdown("## " + s.value)
+        col.image(Flickr8kDataset.get_image_path(s))
+        if src != "image":
+            col.audio(Flickr8kDataset.get_audio_path(s))
         col.code("\n".join(desc))
 
-        st.markdown("top 5 images by audio query")
-        cols = st.beta_columns(5)
-        for r, col in zip(top5, cols):
-            s = samples_img[r]
-            is_correct = s == get_key_img(sample)
-            t = "\n".join("\t" + transcripts[s + "_{}".format(i)] for i in range(5))
-            desc = [
-                "sample:     " + s,
-                "score:      {:.3f}".format(sim[i, r]),
-                "is-correct: " + str(is_correct),
-                "transcripts:" + "\n" + t,
-            ]
-            col.image(Flickr8kDataset.get_image_path(s + "_0"))
-            col.code("\n".join(desc))
+        st.markdown(f"top {k} {tgt} samples by {src} query")
+        for group in partition(5, top_indices):
+            cols = st.beta_columns(5)
+            for r, col in zip(group, cols):
+                t = tgt_samples[r]
+                is_correct = are_same(s, t)
+                desc = [
+                    "sample:     " + t.value,
+                    "score:      {:.3f}".format(sim[i, r]),
+                    "is-correct: " + str(is_correct),
+                    "transcripts:" + get_text(t),
+                ]
+                col.image(Flickr8kDataset.get_image_path(t))
+                col.code("\n".join(desc))
+
         st.markdown("---")
 
 
 def main():
-    audio_to_image_retrieval()
+    st.set_page_config(layout="wide")
+    # evaluate_all_combinations()
+    show_retrieval()
 
 
 if __name__ == "__main__":
