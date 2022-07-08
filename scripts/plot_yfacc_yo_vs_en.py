da24dc319945611d6a8c5833dc583d6dd26808ae
Dan Oneata <dan.oneata@gmail.com>
2022-07-08 17:41:02 +0300

    WIP YFACC analysis

--- scripts/plot_yfacc_yo_vs_en.py
+++ scripts/plot_yfacc_yo_vs_en.py
@@ -6,8 +6,14 @@ import random
 import sys
 
 from matplotlib import pyplot as plt
-from sklearn.metrics import average_precision_score, f1_score
+from sklearn.metrics import (
+    average_precision_score,
+    cohen_kappa_score,
+    confusion_matrix,
+    f1_score,
+)
 
+import pandas
 import numpy as np
 import seaborn as sns
 import streamlit as st
@@ -217,18 +223,25 @@ def show_cross_lingual_differences():
     samples_yo = load_data_yo("yor-5k-init")
     samples_en = load_data_en("en-5k")
 
+    def to_text(a):
+        return " ".join(w.casefold() for _, w in a if w)
+
     for en, yo in join("key", samples_en, "key", samples_yo):
         for w_en, w_yo in vocab:
-            if contains_word(en, w_en) != contains_word(yo, w_yo):
-                print(w_en, contains_word(en, w_en))
-                print(en["alignment"])
-                print(w_yo, contains_word(yo, w_yo))
-                print(yo["alignment"])
+            if w_en != "football":
+                continue
+            # if contains_word(en, w_en) != contains_word(yo, w_yo):
+            if contains_word(en, w_en) or contains_word(yo, w_yo):
+                # print(w_en, contains_word(en, w_en))
+                print(en["key"])
+                print(to_text(en["alignment"]))
+                # print(w_yo, contains_word(yo, w_yo))
+                print(to_text(yo["alignment"]))
                 print()
-                pdb.set_trace()
+                # pdb.set_trace()
 
-    print(len(set(keys_yo) - set(keys_en)))
-    pdb.set_trace()
+    # print(len(set(keys_yo) - set(keys_en)))
+    # pdb.set_trace()
 
 
 def show_cross_lingual_visual_teacher():
@@ -240,6 +253,10 @@ def show_cross_lingual_visual_teacher():
     but the captions are different.
     Even if they are translations of each there are differences.
     Report mean average precision.
+
+    - football / soccer use the same Yoruba keyword, but visually distinct
+    - beach / ocean use also th same Yoruba keyword, but we expect to be correlated visually
+    - words like mountain, bike are more frequent in the Yoruba (maybe because bicycle, top) are mapped to them so the base performance is higher.
 
     """
     st.markdown(description)
@@ -276,11 +293,183 @@ def show_cross_lingual_visual_teacher():
 
     num_en = [sum(true_en[:, w]) for w in range(num_words)]
     num_yo = [sum(true_yo[:, w]) for w in range(num_words)]
-    texts = ["{}\n{} · {}".format(w, e, y) for w, e ,y in zip(words_en, num_en, num_yo)]
+    texts = ["{}\n{} · {}".format(w, e, y) for w, e, y in zip(words_en, num_en, num_yo)]
     # texts = words_en
 
     word_and_results = zip(texts, res_en, res_yo)
     plot(word_and_results)
+
+
+def show_cross_lingual_keyword_correlation():
+    def cohen_kappa_score_max(y1, y2):
+        confusion = confusion_matrix(y1, y2)
+        n_classes = confusion.shape[0]
+        assert n_classes == 2
+
+        p = confusion / confusion.sum()
+        p_row = p.sum(axis=0)
+        p_col = p.sum(axis=1)
+
+        # p_max = p[0, 0] + p[1, 1]
+        p_max = np.sum(np.minimum(p_row, p_col))
+        p_exp = np.sum(p_row * p_col)
+        return (p_max - p_exp) / (1 - p_exp)
+
+    def cohen_kappa_score_within(y1, y2):
+        return cohen_kappa_score(y1, y2) / cohen_kappa_score_max(y1, y2)
+
+    def argsort_sim_mat(sm):
+        idx = [np.argmax(np.sum(sm, axis=1))]  # a
+        for i in range(1, len(sm)):
+            sm_i = sm[idx[-1]].copy()
+            sm_i[idx] = -1
+            idx.append(np.argmax(sm_i))  # b
+        return np.array(idx)
+
+    def get_sim_mat2(sm):
+        n = sm.shape[0]
+        return 1 / (np.linalg.norm(sm[:, np.newaxis] - sm[np.newaxis], axis=-1) + 1 / n)
+
+    description = """
+    ## Cross-lingual keyword correlation
+
+    For each pair of English–Yoruba keyword pair, show the correlation across the test samples.
+    Report the normalize Kappa score.
+
+    """
+    st.markdown(description)
+
+    samples_yo = load_data_yo("yor-5k-init")
+    samples_en = load_data_en("en-5k")
+
+    words_en, words_yo = zip(*vocab)
+    # words_yo = list(set(words_yo))  # unique words
+
+    # Remove nans
+    # idxs_nan = [20, 21, 44, 49, 54]
+    # idxs = set(range(num_words)) - set(idxs_nan)
+    # idxs = random.sample(idxs, 20)
+
+    selected_words = [
+        "beach",
+        "ocean",
+        "sand",
+        "small",
+        "little",
+        "stick",
+        "tree",
+        "white",
+        "red",
+        "tennis",
+        "ball",
+        "stands",
+        "sits",
+        "football",
+        "soccer",
+        "shirt",
+        "wearing",
+        # "water",
+        "boy",
+        "mountain",
+        "top",
+    ]
+    idxs = [words_en.index(w) for w in selected_words]
+
+    words_en = [w for i, w in enumerate(words_en) if i in idxs]
+    words_yo = [w for i, w in enumerate(words_yo) if i in idxs]
+
+    def to_binary(sample, words):
+        return [int(contains_word(sample, word)) for word in words]
+
+    def prepare1(en, yo):
+        true_en = to_binary(en, words_en)
+        true_yo = to_binary(yo, words_yo)
+        return true_en, true_yo
+
+    data = [prepare1(en, yo) for en, yo in join("key", samples_en, "key", samples_yo)]
+
+    true_en, true_yo = zip(*data)
+    true_en = np.vstack(true_en)
+    true_yo = np.vstack(true_yo)
+
+    sm = np.vstack(
+        [
+            [
+                cohen_kappa_score_within(true_en[:, i], true_yo[:, j])
+                for i, en in enumerate(words_en)
+            ]
+            for j, yo in enumerate(words_yo)
+        ]
+    )
+
+    # scores = sm.sum(0) + sm.sum(1)
+    # scores = true_en.sum(0) + true_yo.sum(0)
+    # print(list(zip(scores, words_en)))
+    # idxs = sorted(np.argsort(scores)[-20:])
+    # sm = sm[idxs, :][:, idxs]
+    # words_en_1 = [words_en[i] for i in idxs]
+    # words_yo_1 = [words_yo[i] for i in idxs]
+
+    idxs = argsort_sim_mat(get_sim_mat2(sm))
+    sm = sm[idxs, :][:, idxs]
+
+    words_en_1 = [words_en[i] for i in idxs]
+    words_yo_1 = [words_yo[i] for i in idxs]
+
+    df = pandas.DataFrame(sm, index=words_yo_1, columns=words_en_1)
+    df = df[
+        ~df.index.duplicated(keep="first")
+    ]  # some words in Yoruba appears multiple times
+    df = df.stack().reset_index(name="κ score")
+    df = df.rename(columns={"level_0": "yo", "level_1": "en"})
+    df
+
+    # df = pandas.DataFrame(
+    #     [
+    #         {
+    #             "en": en,
+    #             "yo": yo,
+    #             "κ score": cohen_kappa_score_within(true_en[:, i], true_yo[:, j]),
+    #         }
+    #         for i, en in enumerate(words_en)
+    #         for j, yo in enumerate(words_yo)
+    #         if sum(true_yo[:, j]) > 0
+    #     ]
+    # )
+
+    # df = df.pivot(index="en", columns="yo", values="κ score")
+    # df
+
+    with sns.axes_style(style="whitegrid"):
+        g = sns.relplot(
+            data=df,
+            x="yo",
+            y="en",
+            hue="κ score",
+            size="κ score",
+            palette="vlag",
+            edgecolor=".7",
+            hue_norm=(-1, 1),
+            height=4,
+            sizes=(5, 100),
+            size_norm=(-0.1, 1.0),
+        )
+
+        # Tweak the figure to finalize
+        g.set(aspect="equal")
+        g.despine(left=True, bottom=True)
+        g.ax.margins(0.04)
+
+        for label in g.ax.get_xticklabels():
+            label.set_rotation(90)
+
+        for artist in g.legend.legendHandles:
+            artist.set_edgecolor(".7")
+
+        st.pyplot(g)
+
+        # plt.tight_layout()
+        g.savefig("output/plots/yfacc/cross-lingual-keyword-correlation.pdf")
 
 
 def show_yo_vs_en_keyword_performance():
@@ -306,7 +495,9 @@ def show_yo_vs_en_keyword_performance():
 
 
 def main():
-    show_cross_lingual_visual_teacher()
+    show_cross_lingual_differences()
+    show_cross_lingual_keyword_correlation()
+    # show_cross_lingual_visual_teacher()
 
 
 if __name__ == "__main__":
